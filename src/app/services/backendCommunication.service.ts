d23e43b428e86153e072e1245daee2a740820242
l3miage-sbartaim <sbartaiachraf@gmail.com>
2025-02-27 21:01:02 +0100

    Connection front/back first try

--- src/app/services/backendCommunication.service.ts
+++ src/app/services/backendCommunication.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { LatLng, latLng } from 'leaflet';
 import { Client } from '../../models/interfaces/client.model';
 import { Livreur } from '../../models/interfaces/livreur.model';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 // import { TourneeDetailed } from '../../models/tourneeDetailed.model';
 
 
@@ -10,63 +12,15 @@ import { Livreur } from '../../models/interfaces/livreur.model';
 })
 export class BackendCommunicationService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  async postLivreur(livreur: Livreur) {
-    let body = livreur;
-    console.log('body', body);
-    try {
-      const fetchResponse = await fetch(
-        "http://localhost:8080/api/livreur/postLivreur",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(body)
-        }
-      );
-      console.log('request sent: fetchResponse', fetchResponse);
-      if (!fetchResponse.ok) {
-        console.error('Erreur', fetchResponse.statusText);
-        return;
-      }
-
-    }
-    catch (error) {
-      console.error('Erreur', error);
-      return;
-    }
+  getLivreurs() : Observable<any> {
+    return this.http.get("http://localhost:8080/api/livreur")
   }
 
-  // async submitTourneeDetailed(tourneeDetailed: TourneeDetailed) {
-  //   let body = tourneeDetailed;
-  //   console.log('body', body);
-  //   try {
-  //     const fetchResponse = await fetch(
-  //       "http://localhost:8080/postTourneeDetailed",
-  //       {
-  //         method: "POST",
-  //         headers: {
-  //           "Content-Type": "application/json",
-  //         },
-  //         body: JSON.stringify(body)
-  //       }
-  //     );
-  //     console.log('request sent: fetchResponse', fetchResponse);
-  //     if (!fetchResponse.ok) {
-  //       console.error('Erreur', fetchResponse.statusText);
-  //       return;
-  //     }
-
-  //   }
-  //   catch (error) {
-  //     console.error('Erreur', error);
-  //     return;
-  //   }
-  // }
-  
-
+  postLivreur(livreur : Livreur) : Observable<any> {
+    return this.http.post("http://localhost:8080/api/livreur", livreur)
+  }
 }
 
 
